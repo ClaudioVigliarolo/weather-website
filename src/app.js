c86cecebf518a7a4449d4a97e5be241e3fc1d59e
claudio vigliarolo <claviglia@yahoo.it>
2019-11-22 12:22:41 +0100

    improved app.js

--- src/app.js
+++ src/app.js
@@ -4,8 +4,8 @@ const hbs = require('hbs')
 const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
-const app = express()
-
+const app = express() 
+// 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -18,7 +18,7 @@ hbs.registerPartials(partialsPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
-
+//
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
